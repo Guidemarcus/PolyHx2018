6138f98094ccf2dd4acc0b21d438c90fff9bb244
William Chartier <chartier_william@outlook.fr>
2018-01-21 05:44:55 -0500

    ConnectedUser class

---
+++ AppClient/LoginSystem/ConnectedUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LoginSystem
+{
+    public static class ConnectedUser
+    {
+        public static string AWSAccessToken
+        {
+            get { return AWSAccessToken; }
+            set { AWSAccessToken = value; }
+        }
+       
+        public static string SoftAccessToken 
+        {
+            get { return SoftAccessToken; }
+            set { SoftAccessToken = value; }
+        }
+
+    }
+}
